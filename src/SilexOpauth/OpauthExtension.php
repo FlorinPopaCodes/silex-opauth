9a032085d4c9641bd082321b91587da75f1833d8
Android Build Box <allain.lalonde+androidbuildbox@gmail.com>
2013-02-07 11:37:25 -0500

    $reason is undefined in this context, creating it first means that the Inspector will not complain.

--- src/SilexOpauth/OpauthExtension.php
+++ src/SilexOpauth/OpauthExtension.php
@@ -32,6 +32,8 @@ class OpauthExtension implements ServiceProviderInterface
         $Opauth = new Opauth($config, false );
 
       $response = unserialize(base64_decode( $_POST['opauth'] ));
+
+      $failureReason = null;
       /**
        * Check if it's an error callback
        */
@@ -49,8 +51,8 @@ class OpauthExtension implements ServiceProviderInterface
         if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid'])){
           echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.'."<br>\n";
         }
-        elseif (!$Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason)){
-          echo '<strong style="color: red;">Invalid auth response: </strong>'.$reason.".<br>\n";
+        elseif (!$Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $failureReason)){
+          echo '<strong style="color: red;">Invalid auth response: </strong>'.$failureReason.".<br>\n";
         }
         else{
           echo '<strong style="color: green;">OK: </strong>Auth response is validated.'."<br>\n";
